d7b17fca1b8df5e2ffbec23239c0a1f85aad8204
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-01-06 15:59:10 +0100

    [main] add args fetching and storing in a vector

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+
+    let tab: Vec<_> = env::args().collect();
+
+    println!("Args are :");
+    for argument in &tab {
+        println!("{}", argument);
+    }
+    if tab.len() > 1 {
+        println!("arg tab[1] = {}", tab[1]);
+    }
+
+    let path;
+    if tab.len() > 1 {
+        path = &tab[1];
+    }
+
+//    let mut f = try!(File::open(path));
+//    let mut buffer = Vec::new();
+//
+//    try!(f.read_to_end(&mut buffer));
 }
