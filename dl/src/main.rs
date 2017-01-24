f2ff681b1a930f0d28ac69d5a1599855c47deb42
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-01-24 17:43:11 +0100

    add first line of connection test

--- dl/src/main.rs
+++ dl/src/main.rs
@@ -4,5 +4,12 @@ use std::io::prelude::*;
 use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Start Download Manager");
+
+    //let tab: Vec<_> = env::args().collect();
+    //assert_ne!(tab.len(), 1);
+    let file = File::create("received_file");
+    let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+    let mut buf = vec![] ;
+    let _ = stream.read_to_end(&mut buf).unwrap();
 }
