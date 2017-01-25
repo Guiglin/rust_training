8c02fc8ba8964fde176f813a84d2d415d1d583c1
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-01-25 14:49:08 +0100

    add debug and create file for the downloaded ones

--- dl/src/main.rs
+++ dl/src/main.rs
@@ -8,8 +8,12 @@ fn main() {
 
     //let tab: Vec<_> = env::args().collect();
     //assert_ne!(tab.len(), 1);
-    let file = File::create("received_file");
+    let mut file = File::create("received_file").unwrap();
     let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+    println!("Connected to the host");
     let mut buf = vec![] ;
+    println!("Start downloading the file");
     let _ = stream.read_to_end(&mut buf).unwrap();
+    println!("Write the downloaded file into the disk");
+    file.write_all(&mut buf).unwrap();
 }
