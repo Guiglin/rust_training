088c971b03c0978ffbc8f02308f8abe43219fa6d
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-02-03 18:00:40 +0100

    add use and deal with 0 arg

--- ls/src/main.rs
+++ ls/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut tab: Vec<_> = env::args().collect();
+
+    if tab.len()==1 {
+        tab[1] = "./".to_string();
+    }
 }
