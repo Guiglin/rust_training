97b5d837065b252744b62b21aa7f4fc3fc6c496b
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-02-17 15:51:03 +0100

    handle multiple args

--- ls/src/main.rs
+++ ls/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::env;
+use std::path::Path;
 
 fn main() {
     let tab: Vec<_> = env::args().collect();
@@ -9,6 +10,20 @@ fn main() {
             match entry {
                 Ok(n) => println!("> {:?}", n.path()),
                 Err(err) => println!("Error: {}", err),
+            }
+        }
+    } else {
+        for n in tab[1..].iter() {
+            let dir = Path::new(n);
+            if dir.is_dir() {
+                for entry in fs::read_dir(dir).unwrap() {
+                    match entry {
+                        Ok(i) => println!("> {:?}", i.path()),
+                        Err(err) => println!("Err: {}", err),
+                    }
+                }
+            } else {
+                println!("{:?} unknown file or directory", dir);
             }
         }
     }
