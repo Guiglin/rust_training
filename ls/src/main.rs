a2fd53ae33a211cf869801c94e19c9e285788af4
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-02-17 15:33:22 +0100

    first implementation without args

--- ls/src/main.rs
+++ ls/src/main.rs
@@ -1,12 +1,15 @@
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::fs::File;
+use std::fs;
 use std::env;
 
 fn main() {
-    let mut tab: Vec<_> = env::args().collect();
+    let tab: Vec<_> = env::args().collect();
 
     if tab.len()==1 {
-        tab[0] = "./".to_string();
+        for entry in fs::read_dir("./").unwrap() {
+            match entry {
+                Ok(n) => println!("> {:?}", n.path()),
+                Err(err) => println!("Error: {}", err),
+            }
+        }
     }
 }
