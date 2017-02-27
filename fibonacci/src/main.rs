94ea81451701c0049b650a762addceba1df91b4a
Guilhem Baccialone <guilhem.baccialone@gmail.com>
2017-02-27 22:30:41 +0100

    push fibonacci calculation

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,5 +1,23 @@
 use std::env;
+use std::process;
 
 fn main() {
-    println!("Hello, world!");
+    let tab: Vec<_> = env::args().collect();
+
+    if tab.len() < 1 {
+        println!("Needs at least one arg");
+        process::exit(1);
+    }
+
+    let n: i64 = tab[1].parse().unwrap();
+    let mut temp: i64;
+    let mut i: i64 = 0;
+    let mut j: i64 = 1;
+
+    for k in 0..n {
+        temp = i + j;
+        i = j;
+        j = temp;
+        println!("{} {}", k, j);
+    }
 }
