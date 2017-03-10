8a0f3474368f1edb584ddf5f72c47674ca7e70e5
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-03-10 17:35:26 +0100

    modify struct definition for pointers

--- median_filter/src/main.rs
+++ median_filter/src/main.rs
@@ -1,9 +1,16 @@
 
-struct median_filter_t {
-    window: u8,
-    input: usize,
-    hole: i64,
-    max: i64,
+struct median_filter_t<'a> {
+    window: &'a mut u8,
+    input: &'a mut usize,
+    hole: &'a mut i64,
+    max: &'a mut i64,
+}
+
+fn median_init(f: median_filter_t, w_size: isize) -> i64 {
+    *f.max = 0;
+    *f.hole = 0;
+
+    return 0;
 }
 
 fn main() {
