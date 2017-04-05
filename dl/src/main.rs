72f4b5e21c02583895598f31de063d315911746b
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-04-05 11:41:27 +0200

    use of UDP instead

--- dl/src/main.rs
+++ dl/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::net::TcpStream;
 use std::io::prelude::*;
+use std::net::UdpSocket;
 use std::fs::File;
 
 fn main() {
@@ -9,20 +9,19 @@ fn main() {
     //let tab: Vec<_> = env::args().collect();
     //assert_ne!(tab.len(), 1);
     let mut file = File::create("received_file").unwrap();
-    let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+    let stream = UdpSocket::bind("127.0.0.1:8080").expect("could not bind to address");
     println!("Connected to the host");
     let mut buf = vec![] ;
     println!("Start downloading the file");
-    loop {
-        match stream.read(&mut buf) {
-            Ok(n)=> {
-                if n == 0 {
-                    println!("No data");
-                    break;
-                }
-            },
-            Err(error) => println!("{}", error.to_string()),
-        }
+    let result = stream.recv_from(&mut buf);
+    drop(stream);
+
+    match result {
+        Ok ((size, src)) => {
+            println!("Received data from {}", src);
+            println!("Size received = {}", size);
+        },
+        Err(error) => println!("{}", error.to_string()),
     }
     println!("Write the downloaded file into the disk");
     file.write_all(&mut buf).unwrap();
