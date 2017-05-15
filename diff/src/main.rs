fd73e53f9b41e1387577a03ec61342c79a8c00bf
Guilhem Baccialone <guilhem.baccialone@withings.com>
2017-05-15 16:11:50 +0200

    use difference crate

--- diff/src/main.rs
+++ diff/src/main.rs
@@ -1,6 +1,6 @@
-use std::io::BufReader;
+extern crate difference;
+use difference::Changeset;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
 use std::env;
 
@@ -12,14 +12,13 @@ fn main() {
     assert_ne!(tab.len(), 2);
 
     let mut f = File::open(&tab[0]).unwrap();
-    let mut f = BufReader::new(f);
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer).unwrap();
     let mut f2 = File::open(&tab[1]).unwrap();
-    let mut f2 = BufReader::new(f);
-    let mut counter = 0;
-    for line in f.lines() {
-        if line != f2.lines() {
-            counter +=1;
-        }
-    }
-    println!("{} of differences between {:?}", counter, &tab[1..]);
+    let mut buffer2 = String::new();
+    f2.read_to_string(&mut buffer2).unwrap();
+
+    let changeset = Changeset::new(buffer.as_ref(), buffer2.as_ref(), "");
+
+    println!("{}", changeset);
 }
